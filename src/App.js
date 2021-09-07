604b4516c1fe1b509085cce48372aafe727abd3c
December81 <megastudio@outlook.fr>
2021-09-07 11:09:14 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, {useState} from 'react';
+import 'semantic-ui-css/semantic.min.css';
+// import { Icon } from 'semantic-ui-react';
+
+
+
+function App() {
+  return (
+    
+    <div className="container-fluid color">
+      <div className="row">
+        <div className="col-12 gradient row p-0 m-0">
+          test
+        </div>
+
+        </div>
+      </div>
+    );
+}
+
+export default App;
