a23aba150be35afdd3759c01d552d7f180962b85
December81 <megastudio@outlook.fr>
2021-11-17 00:33:56 +0100

    ok

--- src/component/StepOne.js
+++ src/component/StepOne.js
@@ -15,11 +15,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseApp = initializeApp({
-	apiKey: 'AIzaSyDYRZBURVTMEMq7bEYMqaTmhm6b5sHxJmw',
-
-	authDomain: 'backad-59c60.firebaseapp.com',
-
-	projectId: 'backad-59c60'
+	//config
 });
 const db = getFirestore();
 
